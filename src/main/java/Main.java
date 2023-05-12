fba3a1a3932c17e556c359b874a55827af04f4c8
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2023-05-12 16:02:25 -0400

    idk uwu :3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,15 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.gson.GsonFactory;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            HttpRequest request = new NetHttpTransport().createRequestFactory().buildGetRequest(new GenericUrl("https://www.ikea.com/us/en/meta-data/navigation/stores-detailed.json"));
+            HttpRequest request = new NetHttpTransport().createRequestFactory((HttpRequest request1) -> {
+                request1.setParser(new JsonObjectParser(new GsonFactory()));
+            }).buildGetRequest(new GenericUrl("https://www.ikea.com/us/en/meta-data/navigation/stores-detailed.json"));
             HttpResponse response = request.execute();
             String stores = response.parseAsString();
             System.out.println(stores);
---
+++ src/main/java/Store.java
@@ -0,0 +1,12 @@
+import com.google.api.client.util.Key;
+
+public class Store {
+    @Key
+    private String id;
+    @Key
+    private String displayName;
+    @Key
+    private String lat;
+    @Key
+    private String lng;
+}
