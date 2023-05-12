dc268d926a9fbfa99ac819675961903cb436a775
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2023-05-12 15:03:55 -0400

    idk uwu :3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        try {
+            HttpRequest request = new NetHttpTransport().createRequestFactory().buildGetRequest(new GenericUrl("https://www.ikea.com/us/en/meta-data/navigation/stores-detailed.json"));
+            HttpResponse response = request.execute();
+            String stores = response.parseAsString();
+            System.out.println(stores);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
