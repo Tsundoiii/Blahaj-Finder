5e551189f271a1b27ef902d8f353a49c9a1a07d3
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2023-05-12 23:12:08 -0400

    added parsing for store info uwu :3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,26 @@
+import java.lang.reflect.Type;
+import java.util.List;
+
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.gson.reflect.TypeToken;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            HttpRequest request = new NetHttpTransport().createRequestFactory((HttpRequest request1) -> {
-                request1.setParser(new JsonObjectParser(new GsonFactory()));
+            HttpRequest request = new NetHttpTransport().createRequestFactory((HttpRequest hr) -> {
+                hr.setParser(new JsonObjectParser(new GsonFactory()));
             }).buildGetRequest(new GenericUrl("https://www.ikea.com/us/en/meta-data/navigation/stores-detailed.json"));
             HttpResponse response = request.execute();
-            String stores = response.parseAsString();
-            System.out.println(stores);
+            Type type = new TypeToken<List<Store>>() {}.getType();
+            List<Store> stores = (List<Store>) response.parseAs(type);
+            for (Store store : stores) {
+                System.out.println(store.getPlaceId());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,12 +1,94 @@
+import java.util.List;
+import java.util.Map;
+
 import com.google.api.client.util.Key;
 
 public class Store {
+    public static class Address {
+        @Key
+        private String street;
+        @Key
+        private String city;
+        @Key
+        private String stateProvinceCode;
+        @Key
+        private String zipCode;
+
+        public String toString() {
+            return street + "\n" + city + ", " + stateProvinceCode.substring(2) + " " + zipCode;
+        }
+    }
+
+    public static class Hours {
+        @Key
+        private List<Map<String, String>> normal;
+
+        public List<Map<String, String>> getNormal() {
+            return normal;
+        }
+    }
+
+    public static class BuClassification {
+        @Key
+        private String code;
+        @Key
+        private String name;
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
     @Key
     private String id;
     @Key
-    private String displayName;
+    private String name;
     @Key
     private String lat;
     @Key
     private String lng;
+    @Key
+    private Address address;
+    @Key
+    private Hours hours;
+    @Key
+    private BuClassification buClassification;
+    @Key
+    private String placeId;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Hours getHours() {
+        return hours;
+    }
+
+    public BuClassification getBuClassification() {
+        return buClassification;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
 }
